5a1b8172e7e207a0c8d4c637913ef9ef60bd29b1
Babarrera <babarrera1@cps.edu>
2025-12-10 13:23:10 -0600

    Add while loop to print colors before 'yellow'
    Implemented a while loop to print colors until 'yellow'.

--- 2_while_loops.py
+++ 2_while_loops.py
@@ -4,3 +4,15 @@ colors = ["red", "blue", "green", "yellow", "purple"]
 # Challenge:
 # Use a while loop to print each color UNTIL you find "yellow".
 # Do NOT print "yellow" — stop before it.
+
+index = 0
+while index < len(colors):
+    if colors[index] == "yellow":
+        break
+    print(colors[index])
+    index += 1
+    # index += 1 is shortned for index = index + 1
+#expalanation: 
+# We start with index 0 (first color).
+#while index is less than length of colors list (5):
+#check if color at current index is "yellow":
